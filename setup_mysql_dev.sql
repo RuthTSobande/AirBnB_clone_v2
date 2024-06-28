0c8a1537cc719adf0caa72ac1d0d2bf4a4848d8b
RuthT Sobande <136115703+RuthTSobande@users.noreply.github.com>
2024-06-28 20:04:21 +0100

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,8 @@
--- prepares a MySQL server for the project
-
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+-- create database hbnb_dev_db if not exists
+-- create user hbnb_dev with pwd hbnb_dev_pwd
+-- grant hbnb_dev select on performance_schema
+-- grant all privileges on hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
